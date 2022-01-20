f0cb4178e2a668abdd5c0c58a96883ee9f6041bd
thisbytenico <thisbytenico@gmail.com>
2022-01-20 11:36:07 +0800

    Complete 'how to use'

--- src/pages/howToUse.vue
+++ src/pages/howToUse.vue
@@ -70,3 +70,123 @@
     </div>
   </div>
 </template>
+<style lang="scss" >
+.HowToUse {
+  // background-image: url(../assets/img/how_to_use/bg.png);
+  // background-repeat: no-repeat;
+  // background-position: center 10%;
+  // background-size:cover;
+  // padding-top:1.1rem;
+  height: 100%;
+  .box {
+    width: 14.4rem;
+    margin: 0 auto 0;
+    padding-top: 0.96rem;
+    .h-top {
+      img {
+        width: 4.02rem;
+      }
+      p {
+        width: 10.42rem;
+        height: 1rem;
+        font-size: 0.26rem;
+        font-weight: 400;
+        color: rgba(255, 255, 255, 0.8);
+        line-height: 0.4rem;
+        margin-top: 0.3rem;
+        margin-bottom: 0;
+      }
+    }
+
+    .step {
+      padding-left: 0.4rem;
+      .row {
+        display: flex;
+        //  height: 3.15rem;
+        height: 2.8rem;
+
+        align-items: center;
+        .line {
+          display: flex;
+          flex-direction: column;
+          padding: 0 0.8rem 0 0;
+          height: 100%;
+          .item {
+            flex: 1;
+            width: 0;
+            border-right: 2px dashed #8601ff;
+            border-left: 2px dashed #8601ff;
+            position: relative;
+          }
+
+          .item:nth-child(2) {
+            .circle {
+              width: 0.56rem;
+              height: 0.56rem;
+              background: linear-gradient(154deg, #9601ff 0%, #5000ff 100%);
+              border-radius: 50%;
+              position: absolute;
+              top: -0.28rem;
+              left: -0.28rem;
+              font-size: 0.28rem;
+              color: #fff;
+            }
+          }
+        }
+
+        .content {
+          width: 6.4rem;
+          // height: 2.15rem;
+          height: 2rem;
+
+          background: rgba(255, 255, 255, 0.19);
+          border-radius: 0.25rem;
+          display: flex;
+          justify-content: space-between;
+          padding: 0.3rem;
+          .left {
+            text-align: left;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            padding: 0.1rem 0;
+            p {
+              font-size: 0.3rem;
+              font-weight: 600;
+              color: #ffffff;
+              // line-height: 0.22rem;
+              padding: 0 0.3rem 0 0.1rem;
+            }
+            img {
+              cursor: pointer;
+              width: 2.23rem;
+            }
+          }
+          .right {
+            width: 1.58rem;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            img {
+              // width: 1.4rem;
+              height: 1.1rem;
+            }
+          }
+        }
+      }
+
+      .row:first-child {
+        .line .item:nth-child(1) {
+          border: 0;
+        }
+      }
+      .row:last-child {
+        .line .item:nth-child(2) {
+          border: 0;
+        }
+      }
+    }
+  }
+}
+</style>
